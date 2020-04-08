cf66559f176ecbb6d56ef5e945938a800003316d
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-04-08 19:08:30 +0200

    Add project files.

---
+++ CA200408/Kutya.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200408
+{
+    class Kutya
+    {
+        private string Nev;
+
+        public ConsoleColor Szin { get; }
+
+        #region erre van egyszerűbb syntax
+        //private ConsoleColor _szin;
+        //public ConsoleColor Szin
+        //{
+        //    set { _szin = value; }
+        //    get { return _szin; }
+        //}
+        #endregion
+
+        private DateTime Szul;
+
+        private float suly;
+        public float Suly
+        {
+            set
+            {
+                if (suly != 0 && value > suly * 1.01F)
+                    throw new Exception("ennyit egyszerre nem tud enni egy kutya");
+
+                if (value <= 0)
+                    throw new Exception("a súlyának pozitínak kell lennie");
+
+                if (value > 120)
+                    throw new Exception("ez azért kutyának kicsit sok...");
+
+                suly = value;
+            }
+            get
+            {
+                return suly;
+            }
+        }
+
+        #region Ezt a hülyeséget majd JAVA-ban...
+        //public void SetSuly(float ujSuly)
+        //{
+        //    if (ujSuly <= 0)
+        //        throw new Exception("a súlyának pozitínak kell lennie");
+
+        //    if (ujSuly > 120)
+        //        throw new Exception("ez azért kutyának kicsit sok...");
+
+        //    Suly = ujSuly;
+        //}
+        //public float GetSuly()
+        //{
+        //    return Suly;
+        //}
+        #endregion
+
+        public void Ugat()
+        {
+            Console.ForegroundColor = this.Szin;
+            Console.WriteLine("Vau-vau!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public void Eszik(float mennyiseg)
+        {
+            Suly += mennyiseg;
+            Console.WriteLine($"{Nev} modja: nyami! <3 :3");
+        }
+
+        public Kutya(string nev, int szin, string szul, float suly)
+        {
+            Nev = nev;
+            Szin = (ConsoleColor)szin;
+            Szul = DateTime.Parse(szul);
+
+            Suly = suly;
+        }
+
+    }
+}
---
+++ CA200408/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200408
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kutya[] kt = new Kutya[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                kt[i] = new Kutya("Hektor", 2, "2000-02-02", 10);
+            }
+            
+            var k = new Kutya("Bodri", 2, "2015-03-08", 80);
+
+            k.Eszik(0.7F);
+            k.Ugat();
+            Console.WriteLine(k.Suly);
+            k.Eszik(0.5F);
+
+            Console.WriteLine();
+
+            Console.WriteLine(k.Suly);
+            Console.ReadKey();
+        }
+    }
+}
